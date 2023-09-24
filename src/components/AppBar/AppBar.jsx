504c333518298ba94c7eb93363e3dbabd5f28737
Skaro2336 <matveev.m0612@gmail.com>
2023-09-25 00:15:46 +0300

    fix

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,18 +1,32 @@
+import * as React from 'react';
+import { AppBar as MuiAppBar, Toolbar, Typography, Box } from '@mui/material';
 import Navigation from 'components/Navigation';
 import UserMenu from 'components/UserMenu';
-import AuthNav from 'components/AuthNav';
 import useAuth from 'hooks/useAuth';
 import { Header } from './AppBarStyles';
+import { Link, NavLink } from 'react-router-dom';
 
-const AppBar = () => {
+function AppBar() {
   const { isLoggedIn } = useAuth();
 
   return (
     <Header>
+      <MuiAppBar position="static">
+        <Toolbar>
+          {!isLoggedIn && (
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link to="/">Phonebook</Link>
+            </Typography>
+          )}
+          <Typography variant="h6">
+            {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
+          </Typography>
+          <Box sx={{ marginLeft: 'auto' }}>{isLoggedIn && <UserMenu />}</Box>
+        </Toolbar>
+      </MuiAppBar>
       <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
     </Header>
   );
-};
+}
 
 export default AppBar;
