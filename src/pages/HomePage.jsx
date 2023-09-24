017737930a0c944befc7b8adc30d020c75ce7ebe
Skaro2336 <matveev.m0612@gmail.com>
2023-09-25 00:48:21 +0300

    fix

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginForm from 'components/LoginForm';
+import { Container } from '@mui/material';
+
+const Home = () => (
+  <Container>
+    <LoginForm />
+  </Container>
+);
+
+export default Home;
