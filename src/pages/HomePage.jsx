b82f6e4fa0b17af9853ee1fb30ca77202b91f24e
Skaro2336 <matveev.m0612@gmail.com>
2023-09-25 11:32:44 +0300

    fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import LoginForm from 'components/LoginForm';
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
+import { useSpring, animated } from 'react-spring';
 
-const Home = () => (
-  <Container>
-    <LoginForm />
-  </Container>
-);
+const Home = () => {
+  const springProps = useSpring({
+    from: { opacity: 0, transform: 'translateY(-500px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+    config: { duration: 2000 },
+  });
+
+  return (
+    <animated.div style={springProps}>
+      <Container>
+        <Typography
+          variant="h4"
+          sx={{
+            marginTop: 40,
+            fontWeight: 700,
+            fontSize: 40,
+            textAlign: 'center',
+          }}
+        >
+          Welcome to the Home Page!
+        </Typography>
+      </Container>
+    </animated.div>
+  );
+};
 
 export default Home;
